2c8aa539a5a67605a019a7b46ef51dfdbaed942f
Robert Nyström <golgafrincham@gmail.com>
2022-03-05 22:44:11 +0100

    add ui, slider set luminance

--- monitorcontrol_gui/controller.py
+++ monitorcontrol_gui/controller.py
@@ -1,6 +1,12 @@
 from monitorcontrol_gui.model import Model
+import monitorcontrol as mc
 
 
 class Controller:
     def __init__(self, model: Model) -> None:
         self.model = model
+
+    def set_luminosity(self, value: int) -> None:
+        for monitor_obj in mc.get_monitors():
+            with monitor_obj:
+                monitor_obj.set_luminance(value)
--- monitorcontrol_gui/view.py
+++ monitorcontrol_gui/view.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QMainWindow
 from monitorcontrol_gui.controller import Controller
 from monitorcontrol_gui.model import Model
+from monitorcontrol_gui.ui.main_window import Ui_MainWindow
 
 
 class View(QMainWindow):
@@ -8,3 +9,11 @@ class View(QMainWindow):
         super().__init__()
         self.ctrl = ctrl
         self.model = model
+        print("Setting up UI")
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.buttonSet.clicked.connect(self.click_button)
+
+    def click_button(self) -> None:
+        value = int(self.ui.luminanceValueLabel.text())
+        self.ctrl.set_luminosity(value)
